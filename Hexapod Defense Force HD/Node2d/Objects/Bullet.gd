32777e1423c79349b44150aaa93cb6055b747bc1
sbocaj5 <77814831+sbocaj5@users.noreply.github.com>
2021-11-05 18:37:51 -0600

    Hexopod Defense Force Version .3.1
    - **Mechanic Update**: Hud has been modified to accurately represent the players' health

--- Hexapod Defense Force HD/Node2d/Objects/Bullet.gd
+++ Hexapod Defense Force HD/Node2d/Objects/Bullet.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 signal hit(damage)
 
+
+onready var bulletSprite = get_node("Sprite")
 var gunRotation
 var bulletspeed
 var fallspeed
-
 var bulletTimer = 12.00
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	"""Set bullet speed and time"""
-	bulletspeed = 1500
+	bulletspeed = 800
 	fallspeed = 0
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,12 +22,14 @@ func _process(delta):
 		destroy_self()
 	
 	move_local_x(1*bulletspeed*delta)
-	if bulletspeed > 50:
+	if bulletspeed > 50 and fallspeed < bulletspeed:
 		bulletspeed -= 1
 		fallspeed += .1
-	set_position(Vector2(get_position().x,get_position().y + fallspeed))
 
-
+	var positionChange = Vector2(get_position().x,get_position().y + fallspeed)
+	set_position(positionChange)
+	set_rotation(get_rotation()+delta)
+	
 func _on_Area2D_area_entered(area):
 	"""If the bullet hits something Destroy it"""
 	destroy_self()
